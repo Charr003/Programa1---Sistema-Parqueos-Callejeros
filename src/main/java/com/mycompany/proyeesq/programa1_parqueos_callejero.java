fcf104d18769777d2b0d8517decd2b1ae6d6c1f2
Charr003 <cargutierrez@estudiantec.cr>
2024-10-21 00:25:44 -0600

    Source Commit
    Base del proyecto

---
+++ src/main/java/com/mycompany/proyeesq/programa1_parqueos_callejero.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyeesq;
+
+/**
+ *
+ * @author XPC
+ */
+
+/* Estudiante: Carlos Andrés Gutiérrez González
+   2023396058 
+   Curso: Programación Orientada a Objectos
+   Grupo #2
+    
+    I Proyecto: Programa Parqueos Callejeros
+
+    II SEMESTRE 2024 
+    Profesor: William Mata Rodríguez 
+*/
+
+
+
+public class programa1_parqueos_callejero {
+      
+    
+    public static void main(String[] args) {
+        
+        SistemaIngreso ingreso = new SistemaIngreso(); // Jframe donde se manejara el ingreso, registro y recuperacion de datos
+        ingreso.setVisible(true);
+        ingreso.setLocationRelativeTo(null);       
+ 
+    } 
+}
